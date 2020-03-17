832e928245a9a177d346b091b5c26843c4a6b2b7
Aki Vänttinen <aki.vanttinen@sedgestudios.com>
2020-03-17 21:58:32 +0200

    warn build works for units too

--- sharpy/plans/tactics/warn_build_macro.py
+++ sharpy/plans/tactics/warn_build_macro.py
@@ -32,6 +32,15 @@ class WarnBuildMacro(ActBase):
             if units and len(units) == count:
                 text = f'#{count} {type_id.name} started:{self.ai.time_formatted} target was {self.time_formatted(timing)}'
                 await self.knowledge.chat_manager.chat_taunt_once(str(item), lambda: text)
+
+        for item in self.unit_timings:
+            type_id = item[0]
+            count = item[1]
+            timing = item[2]
+            current_count = self.get_count(type_id, include_killed=True)
+            if count == current_count:
+                text = f'#{count} {type_id.name} started:{self.ai.time_formatted} target was {self.time_formatted(timing)}'
+                await self.knowledge.chat_manager.chat_taunt_once(str(item), lambda: text)
         return True
 
     def time_formatted(self, t: float) -> str:
