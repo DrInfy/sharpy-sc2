c72dc8faf9a249ce6930f78bd056107904a52aaf
Matti Bragge <1806447+merfolk@users.noreply.github.com>
2020-04-19 11:39:35 +0300

    Move script from repository root to tools folder

--- tools/sc2pathlibp-update.py
+++ tools/sc2pathlibp-update.py
@@ -5,12 +5,17 @@ Script to update sc2-pathlib in sc2pathlibp folder.
 import os
 import shutil
 import zipfile
+from pathlib import Path
 from urllib.request import urlretrieve
 from urllib.parse import urlparse
 
 import requests
 import json
 
+# Set working dir to root of repository.
+script_path = Path(os.path.abspath(__file__))
+assert script_path.parent.stem == "tools", "`This script expects to be in tools folder under repository root.`"
+os.chdir(script_path.parent.parent)
 
 latest_release_url = "https://api.github.com/repos/DrInfy/sc2-pathlib/releases/latest"
 sc2pathlibp = "sc2pathlibp"
