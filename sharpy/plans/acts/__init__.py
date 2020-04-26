819cf8c62580afb52b5315b71262d3253a35ae32
Aki Vänttinen <aki.vanttinen@sedgestudios.com>
2020-04-26 18:31:12 +0300

    removed terran planetary act from acts init and optimized imports

--- sharpy/plans/acts/__init__.py
+++ sharpy/plans/acts/__init__.py
@@ -1,20 +1,20 @@
 from .act_base import ActBase
 from .act_building import ActBuilding
-from .expand import Expand, ActExpand
-from .tech import Tech, ActTech
-from .act_unit_once import ActUnitOnce
+from .act_custom import ActCustom
 from .act_many import ActMany
 from .act_unit import ActUnit
+from .act_unit_once import ActUnitOnce
 from .act_zerg_morph import ActMorphBuilding
-from .build_ramp import ActBuildingRamp
-from .grid_building import GridBuilding
-from .morph_warp_gates import MorphWarpGates
+from .build_gas import BuildGas
 from .build_position import BuildPosition
-from sharpy.plans.acts.terran.morph_planetary import MorphPlanetary
+from .build_ramp import ActBuildingRamp
 from .cancel_building import CancelBuilding
-from .defensive_building import DefensiveBuilding
 from .defensive_building import DefensePosition
-from .reserve import Reserve
-from .act_custom import ActCustom
-from .position_building import PositionBuilding
+from .defensive_building import DefensiveBuilding
+from .expand import Expand, ActExpand
+from .grid_building import GridBuilding
 from .methods import merge_to_act
+from .morph_warp_gates import MorphWarpGates
+from .position_building import PositionBuilding
+from .reserve import Reserve
+from .tech import Tech, ActTech
