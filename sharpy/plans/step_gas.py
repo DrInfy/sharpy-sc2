d213f4d7dccb55ec02f3bced957111b0046bc4f1
Aki Vänttinen <aki.vanttinen@sedgestudios.com>
2020-05-09 00:06:56 +0300

    Fixed StepBuildGas to work properly when to_count is chagned on the fly.

--- sharpy/plans/step_gas.py
+++ sharpy/plans/step_gas.py
@@ -7,4 +7,13 @@ class StepBuildGas(Step):
     """With conditions, builds a new gas mining facility closest to vespene geyser with closest worker"""
 
     def __init__(self, to_count: int, requirement=None, skip=None, skip_until=None):
-        super().__init__(requirement, BuildGas(to_count), skip, skip_until)
+        self.build_gas = BuildGas(to_count)
+        super().__init__(requirement, self.build_gas, skip, skip_until)
+
+    @property
+    def to_count(self):
+        return self.build_gas.to_count
+
+    @to_count.setter
+    def to_count(self, value):
+        self.build_gas.to_count = value
