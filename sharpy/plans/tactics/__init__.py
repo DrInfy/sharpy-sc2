49c293b2d5a9603ad08505dcc533e113214637f6
Aki Vänttinen <aki.vanttinen@sedgestudios.com>
2020-06-30 22:45:43 +0300

    unified distribute workers into one

--- sharpy/plans/tactics/__init__.py
+++ sharpy/plans/tactics/__init__.py
@@ -11,4 +11,4 @@ from .warn_build_macro import WarnBuildMacro
 from .worker_counterattack import WorkerCounterAttack
 from .worker_rally_point import WorkerRallyPoint
 from .scouting import Scout, ScoutLocation
-from .distribute_workers2 import PlanDistributeWorkersV2
+from .distribute_workers import DistributeWorkers, PlanDistributeWorkers, PlanDistributeWorkersV2
