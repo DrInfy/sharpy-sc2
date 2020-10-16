fcef512ae29618d0aa93d8e6adc7a4168c1bd4b1
Aki Vänttinen <aki.vanttinen@sedgestudios.com>
2020-10-16 18:18:12 +0300

    Removed gate unit, robo unit and star unit as these are implemented in ProtossUnit

--- sharpy/plans/acts/protoss/__init__.py
+++ sharpy/plans/acts/protoss/__init__.py
@@ -5,10 +5,7 @@ from .chrono_any_tech import ChronoAnyTech
 from .chrono_tech import ChronoTech
 from .chrono_unit import ChronoUnit
 from .defensive_cannons import DefensiveCannons
-from .gate_unit import GateUnit
 from .protoss_unit import ProtossUnit
 from .restore_power import RestorePower
-from .robo_unit import RoboUnit
-from .star_unit import StarUnit
 from .warp_unit import WarpUnit
 from .chrono_building import ChronoBuilding
