d2dacefb648623de58f98ab4482a7ba10d5fdedd
Aki Vänttinen <aki.vanttinen@sedgestudios.com>
2020-12-03 19:22:32 +0200

    fixed act unit once

--- sharpy/plans/acts/act_unit_once.py
+++ sharpy/plans/acts/act_unit_once.py
@@ -4,6 +4,6 @@ from .act_unit import ActUnit
 class ActUnitOnce(ActUnit):
     def get_unit_count(self) -> int:
         count = super().get_unit_count()
-        count += self.knowledge.lost_units_manager.own_lost_type(self.unit_type)
+        count += self.lost_units_manager.own_lost_type(self.unit_type)
 
         return count
