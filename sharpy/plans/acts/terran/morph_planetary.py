fd529100f5fe63fbfa4b6974bc930667b760b827
lladdy <randallboland@bigpond.com>
2022-02-22 19:07:42 +1030

    Fix lint error

--- sharpy/plans/acts/terran/morph_planetary.py
+++ sharpy/plans/acts/terran/morph_planetary.py
@@ -2,6 +2,7 @@ from sc2.ids.ability_id import AbilityId
 from sharpy.plans.acts.morph_building import MorphBuilding
 from sc2.ids.unit_typeid import UnitTypeId
 
+
 class MorphPlanetary(MorphBuilding):
     def __init__(self, target_count: int = 99):
         super().__init__(
