6b0ca27c55d77cc01f2c41321831185a17308a72
Aki Vänttinen <aki.vanttinen@sedgestudios.com>
2023-01-16 00:47:32 +0200

    renamed mine open blocked base file

--- sharpy/plans/acts/__init__.py
+++ sharpy/plans/acts/__init__.py
@@ -19,4 +19,4 @@ from .reserve import Reserve
 from .tech import Tech
 from .workers import Workers
 from .auto_worker import AutoWorker
-from .mine_out_blocked_base import MineOpenBlockedBase
+from .mine_open_blocked_base import MineOpenBlockedBase
